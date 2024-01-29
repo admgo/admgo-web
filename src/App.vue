be71c85c1be3ceb9e01af1cde9c21d23aadd6c23
Kenley <1161723373@qq.com>
2024-01-29 21:13:18 +0800

    init project

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import { Button } from '@/components/ui/button'
+import Login from "@/pages/login.vue";
+</script>
+
+<template>
+  <div>
+<!--    <Button>Click me</Button>-->
+    <login></login>
+  </div>
+</template>
---
+++ src/pages/login.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import {UserAuthForm} from '@/components/login'
+import { cn } from '@/lib/utils'
+import { buttonVariants } from '@/components/ui/button'
+</script>
+
+<template>
+  <div class="md:hidden">
+        <UserAuthForm />
+  </div>
+</template>
