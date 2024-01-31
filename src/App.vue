0e01c24b1d9599b36ed8388715d7a29404e86c3c
Kenley <1161723373@qq.com>
2024-01-31 13:40:23 +0800

    添加路由组件

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,8 @@
 <script setup lang="ts">
-import { Button } from '@/components/ui/button'
-import Login from "@/pages/login.vue";
 </script>
 
 <template>
   <div>
-<!--    <Button>Click me</Button>-->
-    <login></login>
+    <router-view/>
   </div>
 </template>
