255bb5c70e472aca368a9f32e91b6d380ced7be8
Kenley <1161723373@qq.com>
2024-02-04 22:38:24 +0800

    优化代码规范

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,8 @@
-import {createRouter} from 'vue-router'
-import {createWebHistory} from 'vue-router'
-import {routes} from './routes.ts'
+import { createRouter } from 'vue-router'
+import { createWebHistory } from 'vue-router'
+import { routes } from './routes.ts'
 
-export  const router = createRouter({
-    history: createWebHistory(),  // history 模式
-    routes: routes,
+export const router = createRouter({
+  history: createWebHistory(), // history 模式
+  routes: routes,
 })
--- src/router/routes.ts
+++ src/router/routes.ts
@@ -1,8 +1,8 @@
 import { RouteRecordRaw } from 'vue-router'
 import login from '@/pages/login.vue'
-import workbench from '@/pages/workbench.vue'
+import workbench from '@/pages/workspace/workbench.vue'
 
-export const routes:Readonly<RouteRecordRaw[]> = [
-    { path: '/login', component: login },
-    { path: '/workbench', component: workbench },
+export const routes: Readonly<RouteRecordRaw[]> = [
+  { path: '/login', component: login },
+  { path: '/workbench', component: workbench },
 ]
