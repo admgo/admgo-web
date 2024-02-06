43284ee4f555e0b68c056d91f84c10fc697a7885
Kenley <1161723373@qq.com>
2024-02-06 22:35:23 +0800

    修改路由

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -1,8 +1,18 @@
 import { RouteRecordRaw } from 'vue-router'
 import login from '@/pages/login.vue'
+import { Layout } from '@/components/layout'
 import workbench from '@/pages/workspace/workbench.vue'
 
 export const routes: Readonly<RouteRecordRaw[]> = [
   { path: '/login', component: login },
-  { path: '/workbench', component: workbench },
+  {
+    path: '/workbench',
+    component: Layout,
+    children: [
+      {
+        path: '/workbench',
+        component: workbench,
+      },
+    ],
+  },
 ]
