d357727474569c4cc4324c323e5e150a50bb00cb
Kenley <1161723373@qq.com>
2024-02-10 23:41:08 +0800

    格式化代码，解决代码格式警告

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,8 @@
-import { createRouter } from 'vue-router'
+import {
+  createRouter,
+  NavigationGuardNext,
+  RouteLocationNormalized,
+} from 'vue-router'
 import { createWebHistory } from 'vue-router'
 import { routes } from './routes.ts'
 
@@ -6,3 +10,16 @@ export const router = createRouter({
   history: createWebHistory(), // history 模式
   routes: routes,
 })
+
+// 注册全局的导航守卫
+router.beforeResolve(
+  (
+    to: RouteLocationNormalized,
+    from: RouteLocationNormalized,
+    next: NavigationGuardNext,
+  ) => {
+    console.log(to)
+    console.log(from)
+    next()
+  },
+)
--- src/router/routes.ts
+++ src/router/routes.ts
@@ -2,17 +2,44 @@ import { RouteRecordRaw } from 'vue-router'
 import login from '@/pages/login.vue'
 import { Layout } from '@/components/layout'
 import workbench from '@/pages/workspace/workbench.vue'
+import dashboard from '@/pages/workspace/dashboard.vue'
+import NotFound from '@/pages/404.vue'
 
 export const routes: Readonly<RouteRecordRaw[]> = [
-  { path: '/login', component: login },
+  // ===============登录页===============
+  { path: '/login', component: login, name: 'login' },
+  // ===============/===============
+  { path: '/', redirect: { name: 'overview' } },
+  // ===============工作台===============
   {
     path: '/workbench',
+    name: 'workbench',
+    redirect: { name: 'overview' },
     component: Layout,
     children: [
       {
-        path: '/workbench',
+        path: '/workbench/overview',
+        name: 'overview',
         component: workbench,
       },
+      {
+        path: '/workbench/dashboard',
+        name: 'dashboard',
+        component: dashboard,
+      },
     ],
+  },
+  // ===============404===============
+  {
+    // 404
+    path: '/404',
+    name: '404',
+    component: NotFound,
+  },
+  {
+    // 404
+    path: '/:catchAll(.*)',
+    name: 'NotFound',
+    redirect: { name: '404' },
   },
 ]
