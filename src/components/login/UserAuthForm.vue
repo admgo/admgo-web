916e5f452064e1e3728e5ec7e32747e76d1b371f
Kenley <1161723373@qq.com>
2024-02-11 00:06:37 +0800

    添加组件

--- src/components/login/UserAuthForm.vue
+++ src/components/login/UserAuthForm.vue
@@ -1,14 +1,19 @@
 <script setup lang="ts">
 import { ref } from 'vue'
-import { RefreshCw } from 'lucide-vue-next';
-// import GitHubLogo from '~icons/radix-icons/github-logo'
+import { RefreshCw } from 'lucide-vue-next'
 
 import { cn } from '@/lib/utils'
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
 const isLoading = ref(false)
+
+const username = ref()
+const password = ref()
+
 async function onSubmit(event: Event) {
+  console.log(username)
+  console.log(password)
   event.preventDefault()
   isLoading.value = true
 
@@ -23,27 +28,27 @@ async function onSubmit(event: Event) {
     <form @submit="onSubmit">
       <div class="grid gap-2">
         <div class="grid gap-1">
-          <Label class="sr-only" for="email">
-            Email
-          </Label>
+          <Label class="sr-only" for="email"> Email </Label>
           <Input
-              id="email"
-              placeholder="用户名"
-              type="text"
-              auto-capitalize="none"
-              auto-complete="email"
-              auto-correct="off"
-              :disabled="isLoading"
+            id="email"
+            v-model="username"
+            placeholder="用户名"
+            type="text"
+            auto-capitalize="none"
+            auto-complete="email"
+            auto-correct="off"
+            :disabled="isLoading"
           />
           <Input
-              class="my-2"
-              id="email"
-              placeholder="密码"
-              type="email"
-              auto-capitalize="none"
-              auto-complete="email"
-              auto-correct="off"
-              :disabled="isLoading"
+            id="email"
+            v-model="password"
+            class="my-2"
+            placeholder="密码"
+            type="email"
+            auto-capitalize="none"
+            auto-complete="email"
+            auto-correct="off"
+            :disabled="isLoading"
           />
         </div>
         <Button :disabled="isLoading">
@@ -57,14 +62,10 @@ async function onSubmit(event: Event) {
         <span class="w-full border-t" />
       </div>
       <div class="relative flex justify-center text-xs uppercase">
-        <span class="bg-background px-2 text-muted-foreground">
-          或使用
-        </span>
+        <span class="bg-background px-2 text-muted-foreground"> 或使用 </span>
       </div>
     </div>
     <Button variant="outline" type="button" :disabled="isLoading">
-      <LucideSpinner v-if="isLoading" class="mr-2 h-4 w-4 animate-spin" />
-      <GitHubLogo v-else class="mr-2 h-4 w-4" />
       LDAP
     </Button>
   </div>
