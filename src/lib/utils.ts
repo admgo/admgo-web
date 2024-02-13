4adc942c8d367e6eef1c63ccb56d355432c44560
Kenley <1161723373@qq.com>
2024-02-14 00:07:59 +0800

    添加修改窗口标题的方法

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -5,3 +5,14 @@ import { twMerge } from 'tailwind-merge'
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+/**
+ * 修改窗口的标题.
+ *
+ * @param title - 标题
+ *
+ */
+export const setTitle = (title: string | undefined) => {
+  window.document.title =
+    title == undefined ? 'admgo' : title == '' ? 'admgo' : title
+}
