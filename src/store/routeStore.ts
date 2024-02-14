053b476989982d097f63bcd8da63a6592447ea02
Kenley <1161723373@qq.com>
2024-02-14 10:59:31 +0800

    重构routerStore的逻辑

--- src/store/routeStore.ts
+++ src/store/routeStore.ts
@@ -1,22 +1,27 @@
 import { defineStore } from 'pinia'
-import { reactive } from 'vue'
-import { RouteRecordRaw } from 'vue-router'
+import { ref } from 'vue'
 import { router } from '@/router'
+import { Layout } from '@/components/layout'
 
 export const useRouteStore = defineStore('store', () => {
-  let route: RouteRecordRaw = reactive({
-    path: '/workbench',
-    name: 'workbench',
-    component: () => import('@/pages/workspace/dashboard.vue'),
-  })
-  const getRoute = () => {
+  let isAddRoutes = ref(false)
+  const syncRoute = () => {
     // 异步获取路由信息
-    return router
+    router.addRoute({
+      path: '/cmdb',
+      name: 'cmdb',
+      redirect: { name: 'cmdb-overview' },
+      component: Layout,
+      children: [
+        {
+          path: '/cmdb/overview',
+          name: 'cmdb-overview',
+          component: () => import('../pages/cmdb/overview.vue'),
+        },
+      ],
+    })
+    isAddRoutes.value = true
   }
 
-  const changeRoute = (newRoute: RouteRecordRaw) => {
-    route = newRoute
-  }
-
-  return { route, getRoute, changeRoute }
+  return { isAddRoutes, syncRoute }
 })
