8405a7158e9c872272fb507f5aac7a4761871655
Kenley Wang <1161723373@qq.com>
2024-04-06 20:19:25 +0800

    添加测试页

---
+++ src/pages/test.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+import {
+  Message,
+  MessageDescription,
+  MessageTitle,
+} from '@/components/ui/message'
+import { useColorMode } from '@vueuse/core'
+import { Button } from '@/components/ui/button'
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu'
+const mode = useColorMode()
+</script>
+
+<template>
+  <Message>
+    <MessageTitle>Login Success</MessageTitle>
+    <!--    <MessageDescription> login success </MessageDescription>-->
+  </Message>
+
+  <DropdownMenu>
+    <DropdownMenuTrigger as-child>
+      <Button variant="outline"> change zhuti </Button>
+    </DropdownMenuTrigger>
+    <DropdownMenuContent align="end">
+      <DropdownMenuItem @click="mode = 'light'"> Light </DropdownMenuItem>
+      <DropdownMenuItem @click="mode = 'dark'"> Dark </DropdownMenuItem>
+      <DropdownMenuItem @click="mode = 'auto'"> System </DropdownMenuItem>
+    </DropdownMenuContent>
+  </DropdownMenu>
+</template>
+
+<style scoped></style>
