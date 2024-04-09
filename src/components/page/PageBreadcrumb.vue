5ed1ca487324db35adb55a9327c649d9f510f4b3
Kenley Wang <1161723373@qq.com>
2024-04-09 20:53:16 +0800

    add page components

---
+++ src/components/page/PageBreadcrumb.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu'
+
+import { BreadcrumbItem } from '@/components/ui/breadcrumb'
+import { ChevronDown } from 'lucide-vue-next'
+</script>
+
+<template>
+  <BreadcrumbItem>
+    <DropdownMenu>
+      <DropdownMenuTrigger class="flex items-center gap-1">
+        Components
+        <ChevronDown />
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="start">
+        <DropdownMenuItem>Documentation</DropdownMenuItem>
+        <DropdownMenuItem>Themes</DropdownMenuItem>
+        <DropdownMenuItem>GitHub</DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  </BreadcrumbItem>
+</template>
